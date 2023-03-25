3e4dc61c9114a50436a8b9af5dcbf2d6f1890038
s2000promax <s2000promax@gmail.com>
2023-03-26 00:49:15 +0400

    fix: styles

--- src/shared/uiKit/Icon/ui/Icon.tsx
+++ src/shared/uiKit/Icon/ui/Icon.tsx
@@ -11,6 +11,6 @@ export const Icon = memo((props: IconProps) => {
   const { className, Svg } = props;
 
   return (
-    <Svg className={classNames(cls.Icon, {}, [className])} />
+    <Svg className={classNames(cls.icon, {}, [className])} />
   );
 });
